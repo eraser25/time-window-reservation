a490d9cadf0a98763f797723fd548f33a6e470ff
eraser25 <140506416+eraser25@users.noreply.github.com>
2026-03-25 15:30:45 +0300

    Delete time-window-reservation directory

--- time-window-reservation/templates/countdown-timer.php
+++
@@ -1,6 +0,0 @@
-<div class="twrf-countdown" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-start="<?php echo esc_attr( $reservation->reservation_start ); ?>" data-end="<?php echo esc_attr( $reservation->reservation_end ); ?>">
-	<h3><?php _e( 'Reservation Countdown', 'twrf' ); ?></h3>
-	<div class="countdown-display">
-		<span class="countdown-text"></span>
-	</div>
-</div>
--- time-window-reservation/templates/participant-count.php
+++
@@ -1,3 +0,0 @@
-<div class="twrf-participant-count" data-product-id="<?php echo esc_attr( $product_id ); ?>">
-	<p><?php _e( 'Participants: ', 'twrf' ); ?><span class="count"><?php echo Reservation_Manager::get_participant_count( $reservation->id ); ?></span>/<?php echo esc_html( $reservation->stock_available ); ?></p>
-</div>
